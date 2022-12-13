b2cdf519000b4b58f42bdf171422a6486e781a31
crabby73 <ruedikrabbe@web.de>
2022-12-13 06:56:46 +0100

    Solved task 1 of day 3

--- aoc2022_day3.py
+++ aoc2022_day3.py
@@ -1,18 +1,62 @@
 import string
 
 lines = open("aoc2022_day3_input.txt", "r").readlines()
+#print(len(lines))
 
 letters = list(string.ascii_letters)
+#print(len(letters))
+#testline = 'tJHJvLJVVttNsvTtTvgHHSVwCsQRQQZCZZMqQMQBnqBMQs\n'
+#testline = testline[:-1]
+#print(testline)
+
+priority = 0
+noLines = 0
+groupMembers = 0
+group = list();
 
 for line in lines:
-    for letter in letters:
-        if line.count(letter) > 1:
-            
-            
+    line = line[:-1]
+    numberOfLetters = len(line)
+    if numberOfLetters % 2 != 0:
+        print("ODD!")
+    #print(numberOfLetters)
+    middleIndex = numberOfLetters//2
+    #print(middleIndex)
+    lastIndex = numberOfLetters
+    #print(lastIndex)
+    #print(len(testline))
+    firstPart = line[0:middleIndex]
+    #print(len(firstPart))
+    #print(firstPart)
+    secondPart = line[middleIndex:lastIndex]
+    #print(len(secondPart))
+    #print(secondPart)
     
-# Split line
-# Find same letter in both parts
-# Assign priorty of dublicate letter
-# Sum up priorities
+    if len(firstPart) != len(secondPart):
+        print("Not the same!")
+    
+    found = False
+    for letter in letters:
+        if firstPart.find(letter) != -1 and secondPart.find(letter) != -1:
+            #print(letter)
+            priority = priority + letters.index(letter) + 1
+            noLines = noLines + 1
+            found = True
+     
+    if found == False:
+        print(line)    
+     
+    #if groupMembers < 3:
+    #    group.append(line)
+    #    groupMembers = groupMembers + 1
+    
+    #if groupMembers == 3:
+        
+    
+    
+           
+print(priority)
+#print(noLines)
+
 
  
