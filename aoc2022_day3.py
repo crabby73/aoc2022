5fdb015be29ae3f5059390a7e703c31d7150286f
crabby73 <ruedikrabbe@web.de>
2022-12-20 13:15:44 +0100

    Added solution for part two of day 3

--- aoc2022_day3.py
+++ aoc2022_day3.py
@@ -1,62 +1,62 @@
 import string
 
 lines = open("aoc2022_day3_input.txt", "r").readlines()
-#print(len(lines))
-
 letters = list(string.ascii_letters)
-#print(len(letters))
-#testline = 'tJHJvLJVVttNsvTtTvgHHSVwCsQRQQZCZZMqQMQBnqBMQs\n'
-#testline = testline[:-1]
-#print(testline)
 
-priority = 0
-noLines = 0
-groupMembers = 0
-group = list();
+def solvePartOne(lines, letters):
+    priority = 0
 
-for line in lines:
-    line = line[:-1]
-    numberOfLetters = len(line)
-    if numberOfLetters % 2 != 0:
-        print("ODD!")
-    #print(numberOfLetters)
-    middleIndex = numberOfLetters//2
-    #print(middleIndex)
-    lastIndex = numberOfLetters
-    #print(lastIndex)
-    #print(len(testline))
-    firstPart = line[0:middleIndex]
-    #print(len(firstPart))
-    #print(firstPart)
-    secondPart = line[middleIndex:lastIndex]
-    #print(len(secondPart))
-    #print(secondPart)
-    
-    if len(firstPart) != len(secondPart):
-        print("Not the same!")
-    
-    found = False
-    for letter in letters:
-        if firstPart.find(letter) != -1 and secondPart.find(letter) != -1:
-            #print(letter)
-            priority = priority + letters.index(letter) + 1
-            noLines = noLines + 1
-            found = True
-     
-    if found == False:
-        print(line)    
-     
-    #if groupMembers < 3:
-    #    group.append(line)
-    #    groupMembers = groupMembers + 1
-    
-    #if groupMembers == 3:
+    for line in lines:
+        line = line[:-1]
+        numberOfLetters = len(line)
+
+        if numberOfLetters % 2 != 0:
+            print("ODD!")
+
+        middleIndex = numberOfLetters//2
+        lastIndex = numberOfLetters
+        firstPart = line[0:middleIndex]
+        secondPart = line[middleIndex:lastIndex]
         
+        if len(firstPart) != len(secondPart):
+            print("Not the same!")
+        
+        found = False
+        for letter in letters:
+            if firstPart.find(letter) != -1 and secondPart.find(letter) != -1:
+                priority = priority + letters.index(letter) + 1
+                    
+    return priority  
+
+def solvePartTwo(lines, letters):
+    group = [0, 0, 0]
+    groupMembers = 0
+    priority = 0
     
+    for line in lines:
+        line = line[:-1]
+        if groupMembers < 3:
+            group[groupMembers] = line
+            groupMembers = groupMembers + 1
     
-           
-print(priority)
-#print(noLines)
+        if groupMembers == 3:
+            groupMembers = 0
+            for letter in letters:
+                groupMemberOneLetter = group[0].find(letter)
+                groupMemberTwoLetter = group[1].find(letter)
+                groupMemberThreeLetter = group[2].find(letter)
+                
+                if groupMemberOneLetter != -1 and \
+                   groupMemberTwoLetter != -1 and \
+                   groupMemberThreeLetter != -1:
+                       priority = priority + letters.index(letter) + 1
+            
+    return priority
+
+resultPartOne = solvePartOne(lines, letters)
+print(resultPartOne)
+resultPartTwo = solvePartTwo(lines, letters)
+print(resultPartTwo)
 
 
  
