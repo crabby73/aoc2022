663ae1254f22e062bf080b630bfc43dd1c903bce
crabby73 <ruedikrabbe@web.de>
2022-12-20 15:25:03 +0100

    Started solving day 4 part 1

---
+++ aoc2022_day4.py
@@ -0,0 +1,18 @@
+lines = open("aoc2022_day4_input.txt", "r").readlines()
+
+def solvePartOne(lines):
+     
+    for line in lines:
+        line = line[:-1]
+        transformedLine = transformLine(line)        
+       
+            
+    result = 0
+    return result
+
+def transformLine(line):
+    transformedLine = line
+    return transformedLine
+
+resultPartOne = solvePartOne(lines)
+print(resultPartOne)
